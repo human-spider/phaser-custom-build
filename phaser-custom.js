76ab2e4a61d5054662efcc332f836e2e89c33176
Mykhailo Gryshchenko <mykhailogryshchenko@cloud.upwork.com>
2024-07-23 18:46:18 +0300

    initial

---
+++ phaser-custom.js
@@ -0,0 +1,116 @@
+require('polyfills');
+
+var CONST = require('const');
+var Extend = require('utils/object/Extend');
+
+var Phaser = {
+    Actions: {
+        GridAlign: require('actions/GridAlign'),
+    },
+    Animations: require('animations'),
+    BlendModes: require('renderer/BlendModes'),
+    Cache: require('cache'),
+    Cameras: { Scene2D: require('cameras/2d') },
+    Core: require('core'),
+    Class: require('utils/Class'),
+    Data: require('data'),
+    Display: { Masks: require('display/mask') },
+    DOM: require('dom'),
+    Events: require('events'),
+    FX: require('fx'),
+    Game: require('core/Game'),
+    GameObjects: {
+        DisplayList: require('gameobjects/DisplayList'),
+        GameObjectCreator: require('gameobjects/GameObjectCreator'),
+        GameObjectFactory: require('gameobjects/GameObjectFactory'),
+        UpdateList: require('gameobjects/UpdateList'),
+        // Components: require('gameobjects/components'),
+        BuildGameObject: require('gameobjects/BuildGameObject'),
+        BuildGameObjectAnimation: require('gameobjects/BuildGameObjectAnimation'),
+        GameObject: require('gameobjects/GameObject'),
+        Graphics: require('gameobjects/graphics/Graphics'),
+        Image: require('gameobjects/image/Image'),
+        Layer: require('gameobjects/layer/Layer'),
+        Sprite: require('gameobjects/sprite/Sprite'),
+        Text: require('gameobjects/text/Text'),
+        Group: require('gameobjects/group/Group'),
+        Rectangle: require('gameobjects/shape/rectangle/Rectangle'),
+        Factories: {
+            Graphics: require('gameobjects/graphics/GraphicsFactory'),
+            Image: require('gameobjects/image/ImageFactory'),
+            Layer: require('gameobjects/layer/LayerFactory'),
+            Sprite: require('gameobjects/sprite/SpriteFactory'),
+            Text: require('gameobjects/text/TextFactory'),
+            Group: require('gameobjects/group/GroupFactory'),
+            Rectangle: require('gameobjects/shape/rectangle/RectangleFactory'),
+        },
+        Creators: {
+            Graphics: require('gameobjects/graphics/GraphicsCreator'),
+            Image: require('gameobjects/image/ImageCreator'),
+            Layer: require('gameobjects/layer/LayerCreator'),
+            Sprite: require('gameobjects/sprite/SpriteCreator'),
+            Text: require('gameobjects/text/TextCreator'),
+            Group: require('gameobjects/group/GroupCreator'),
+        }
+    },
+    Geom: {
+        Rectangle: require('geom/rectangle'),
+    },
+    Input: {
+        CreatePixelPerfectHandler: require('input/CreatePixelPerfectHandler'),
+        CreateInteractiveObject: require('input/CreateInteractiveObject'),
+        InputManager: require('input/InputManager'),
+        InputPlugin: require('input/InputPlugin'),
+        InputPluginCache: require('input/InputPluginCache'),
+        Mouse: require('input/mouse'),
+        Pointer: require('input/Pointer'),
+        Touch: require('input/touch')
+    },
+    Loader: {
+        FileTypes: {
+            // AnimationJSONFile: require('loader/filetypes/AnimationJSONFile'),
+            // AtlasJSONFile: require('loader/filetypes/AtlasJSONFile'),
+            // AudioFile: require('loader/filetypes/AudioFile'),
+            // AudioSpriteFile: require('loader/filetypes/AudioSpriteFile'),
+            // HTML5AudioFile: require('loader/filetypes/HTML5AudioFile'),
+            ImageFile: require('loader/filetypes/ImageFile'),
+            // JSONFile: require('loader/filetypes/JSONFile'),
+            // MultiAtlasFile: require('loader/filetypes/MultiAtlasFile'),
+            // PluginFile: require('loader/filetypes/PluginFile'),
+            // ScriptFile: require('loader/filetypes/ScriptFile'),
+            SpriteSheetFile: require('loader/filetypes/SpriteSheetFile'),
+            // TextFile: require('loader/filetypes/TextFile'),
+            // XMLFile: require('loader/filetypes/XMLFile')
+        },
+        File: require('loader/File'),
+        FileTypesManager: require('loader/FileTypesManager'),
+        GetURL: require('loader/GetURL'),
+        LoaderPlugin: require('loader/LoaderPlugin'),
+        // MergeXHRSettings: require('loader/MergeXHRSettings'),
+        // MultiFile: require('loader/MultiFile'),
+        // XHRLoader: require('loader/XHRLoader'),
+        // XHRSettings: require('loader/XHRSettings')
+    },
+    Math: require('math'),
+    Plugins: require('plugins'),
+    Renderer: require('renderer'),
+    Scale: require('scale'),
+    ScaleModes: require('renderer/ScaleModes'),
+    Scene: require('scene/Scene'),
+    Scenes: require('scene'),
+    Structs: require('structs'),
+    Textures: require('textures'),
+    Time: require('time'),
+    Tweens: require('tweens'),
+    Utils: require('utils')
+};
+
+//  Merge in the consts
+
+Phaser = Extend(false, Phaser, CONST);
+
+//  Export it
+
+module.exports = Phaser;
+
+global.Phaser = Phaser;
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    //   mode: 'development',
+    mode: "production",
+
+    stats: {
+        modules: true,
+    },
+
+    entry: {
+        "phaser-custom": `./phaser-custom.js`,
+        "phaser-custom.min": "./phaser-custom.js",
+    },
+
+    resolve: {
+        alias: {
+            eventemitter3: path.resolve(
+                __dirname,
+                "./node_modules/eventemitter3",
+            ),
+        },
+        modules: ["node_modules/phaser/src"],
+    },
+
+    output: {
+        path: `${__dirname}/dist/`,
+        filename: "[name].js",
+        library: "Phaser",
+        libraryTarget: "umd",
+        sourceMapFilename: "[file].map",
+        devtoolModuleFilenameTemplate: "webpack:///[resource-path]",
+        devtoolFallbackModuleFilenameTemplate:
+            "webpack:///[resource-path]?[hash]",
+        umdNamedDefine: true,
+    },
+
+    performance: { hints: false },
+
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                include: /\.min\.js$/,
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            "typeof CANVAS_RENDERER": JSON.stringify(true),
+            "typeof WEBGL_RENDERER": JSON.stringify(true),
+            "typeof WEBGL_DEBUG": JSON.stringify(false),
+            "typeof EXPERIMENTAL": JSON.stringify(false),
+            "typeof PLUGIN_3D": JSON.stringify(false),
+            "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
+            "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
+            "typeof FEATURE_SOUND": JSON.stringify(true),
+        }),
+    ],
+
+    devtool: "source-map",
+};
